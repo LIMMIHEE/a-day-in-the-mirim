09c396938aa0133e52aa13c415316a0379f2335b
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-02-11 00:15:47 +0900

    add Roulette

---
+++ a_day_in_the_MIRIM/Assets/Scenes/Roulette.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roulette : MonoBehaviour
+{
+    float roultteSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //클릭시 회전
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("asdf");
+            this.roultteSpeed = 30;
+        }
+        transform.Rotate(0, 0, this.roultteSpeed);
+
+        this.roultteSpeed *= 0.99f;
+        
+    }
+}
