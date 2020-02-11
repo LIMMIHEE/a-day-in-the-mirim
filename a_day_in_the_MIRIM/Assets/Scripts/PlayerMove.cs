2282e26e4a621e29fb34d3a99defb340e6dd6437
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-02-11 15:40:23 +0900

    200211임미

---
+++ a_day_in_the_MIRIM/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    public float JumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //점프
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("Jumping"))
+        {
+            rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            anim.SetBool("Jumping", true);
+        }
+
+
+        //  멈추게할때 스피드 낮추는 거
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        // 캐릭터 좌우 방향 바꾸기
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+
+        //캐릭터 애니메이션 걷느냐에 따라 바꾸기
+        if (Mathf.Abs( rigid.velocity.x) < 0.4)
+        {
+            anim.SetBool("Walking", false);
+        }
+        else
+        {
+            anim.SetBool("Walking", true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 키로 움직임 컨트롤
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rigid.AddForce(Vector3.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed) //오른쪽 최대속도
+        {
+            rigid.velocity = new Vector3(maxSpeed, rigid.velocity.y);
+        }
+        else if (rigid.velocity.x < maxSpeed*(-1))//왼쪽 최대속도
+        {
+            rigid.velocity = new Vector3(maxSpeed * (-1), rigid.velocity.y);
+        }
+
+
+        //점프하고 돌아옥기?
+        if(rigid.velocity.y < 0) { 
+            //Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1 , LayerMask.GetMask("Platform"));
+        
+            if(rayHit.collider != null)
+            {
+                if(rayHit.distance < 0.5f)
+                {
+                    anim.SetBool("Jumping", false);
+                }
+            }
+        }
+    }
+}
