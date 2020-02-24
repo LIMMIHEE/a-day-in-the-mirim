3a44acd0c6573fdc48b4b72f810d6f1bed492deb
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-02-24 21:50:57 +0900

    방과후 B : 이미지 게임 완성

---
+++ a_day_in_the_MIRIM/Assets/Scripts/GameControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] pictures;
+
+    [SerializeField]
+    public GameObject panel;
+
+    public static bool Finish;
+
+    private void Start()
+    {
+        panel.SetActive(false);
+        Finish = false;
+    }
+
+    private void Update()
+    {
+        if (pictures[0].rotation.z == 0 &&
+           pictures[1].rotation.z == 0 &&
+           pictures[2].rotation.z == 0 &&
+           pictures[3].rotation.z == 0 &&
+           pictures[4].rotation.z == 0 &&
+           pictures[5].rotation.z == 0 &&
+           pictures[6].rotation.z == 0 &&
+           pictures[7].rotation.z == 0 &&
+           pictures[8].rotation.z == 0)
+        {
+            Finish = true;
+            panel.SetActive(true);
+        }
+    }
+}
---
+++ a_day_in_the_MIRIM/Assets/Scripts/TouchRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (!GameControl.Finish)
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
+}
